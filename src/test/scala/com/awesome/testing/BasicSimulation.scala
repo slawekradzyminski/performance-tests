58374cffffac8198e25c6cce3c9579bdbceaeab5
Slawek <s.radzyminski@ocado.com>
2021-10-25 11:25:14 +0200

    Initial version set

--- src/test/scala/com/awesome/testing/BasicSimulation.scala
+++ src/test/scala/com/awesome/testing/BasicSimulation.scala
@@ -1,18 +1,28 @@
 package com.awesome.testing
 
-import com.awesome.testing.core.HttpConfig.httpProtocol
-import com.awesome.testing.scenario.TrainingScenario.trainingScenario
+import com.awesome.testing.config.LocalConfig
 import io.gatling.core.Predef._
+import io.gatling.http.Predef._
 
-import scala.concurrent.duration._
 import scala.language.postfixOps
 
 class BasicSimulation extends Simulation {
 
+  private final val json = "application/json"
+
   setUp(
-//    trainingScenario.inject(atOnceUsers(1))
-    trainingScenario.inject(rampUsers(200) during(60 seconds))
-      .protocols(httpProtocol)
+    scenario("Training scenarion")
+      .exec(
+        http("Admin login request")
+          .post("/users/signin")
+          .body(ElFileBody("bodies/adminLogin.json")).asJson
+          .check(status.is(200))
+      )
+      .inject(atOnceUsers(1))
+      .protocols(http
+        .baseUrl(LocalConfig.BaseUrl)
+        .header("Accept", json)
+        .header("Content-Type", json))
   ).assertions(
     global.responseTime.max.lt(3000),
     global.successfulRequests.percent.is(100)
