ca4e05821c5f3815b2dd5531a001f6d6d60fe523
Slawek <s.radzyminski@ocado.com>
2022-02-23 12:15:16 +0100

    register

--- src/test/scala/com/awesome/testing/scenario/TrainingScenario.scala
+++ src/test/scala/com/awesome/testing/scenario/TrainingScenario.scala
@@ -1,13 +1,21 @@
 package com.awesome.testing.scenario
 
+import com.awesome.testing.feeder.CredentialsFeeder.credentialsFeeder
 import com.awesome.testing.request.LoginRequest.loginRequest
+import com.awesome.testing.request.RegisterRequest.registerRequest
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
+
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 object TrainingScenario {
 
   val trainingScenario: ScenarioBuilder = scenario("Training scenario")
-    .feed(csv("data/users.csv").circular)
+    .feed(credentialsFeeder)
+    .exec(registerRequest)
+    .exitHereIfFailed
+    .pause(5 seconds)
     .exec(loginRequest)
 
 }
