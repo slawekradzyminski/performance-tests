7a3b362f8b31914bd648765d9138466b0f280b6b
Slawek <s.radzyminski@ocado.com>
2022-05-18 11:12:33 +0200

    initial refactor

---
+++ src/test/scala/com/awesome/testing/core/FunctionalAssertions.scala
@@ -0,0 +1,13 @@
+package com.awesome.testing.core
+
+import io.gatling.core.Predef._
+
+object FunctionalAssertions {
+
+  val assertions: Seq[Assertion] = Seq(
+    global.responseTime.max.lt(3000),
+    global.successfulRequests.percent.is(100)
+  )
+
+
+}
---
+++ src/test/scala/com/awesome/testing/scenario/TrainingScenario.scala
@@ -0,0 +1,14 @@
+package com.awesome.testing.scenario
+
+import com.awesome.testing.request.Login.loginRequest
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+
+object TrainingScenario {
+
+  val trainingScenario: ScenarioBuilder = scenario("Training scenarion")
+    .exec(
+      loginRequest
+    )
+
+}
