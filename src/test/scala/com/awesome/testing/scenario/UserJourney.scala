7200f30cba3e8426f9e5f912545202843c68374d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-07 12:02:49 +0200

    02

--- src/test/scala/com/awesome/testing/scenario/UserJourney.scala
+++ src/test/scala/com/awesome/testing/scenario/UserJourney.scala
@@ -1,18 +1,13 @@
 package com.awesome.testing.scenario
 
+import com.awesome.testing.request.LoginRequest.loginRequest
 import io.gatling.core.Predef._
-import io.gatling.http.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 
 object UserJourney {
 
   val testWarezTestScenario: ScenarioBuilder =
     scenario("Training scenario")
-      .exec(
-        http("Admin login request")
-          .post("/users/signin")
-          .body(ElFileBody("bodies/adminLogin.json")).asJson
-          .check(status.is(200))
-      )
+      .exec(loginRequest)
 
 }
